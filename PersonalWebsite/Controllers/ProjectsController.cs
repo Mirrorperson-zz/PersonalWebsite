98e5e5886df6f9bb2a379dc32f6c37912f9eab49
Mirrorperson <Mirrorperson@users.noreply.github.com>
2018-07-01 01:34:54 +0100

    Add new ProjectsController

---
+++ PersonalWebsite/Controllers/ProjectsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PersonalWebsite.Models;
+
+namespace PersonalWebsite.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Projects
+        public ActionResult Index()
+        {
+            return View(db.ProjectModels.ToList());
+        }
+
+        // GET: Projects/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectModel projectModel = db.ProjectModels.Find(id);
+            if (projectModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectModel);
+        }
+
+        // GET: Projects/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Projects/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id, projectTitle, projectDescription")] ProjectModel projectModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ProjectModels.Add(projectModel);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(projectModel);
+        }
+
+        // GET: Projects/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectModel projectModel = db.ProjectModels.Find(id);
+            if (projectModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectModel);
+        }
+
+        // POST: Projects/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id")] ProjectModel projectModel)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(projectModel).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(projectModel);
+        }
+
+        // GET: Projects/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ProjectModel projectModel = db.ProjectModels.Find(id);
+            if (projectModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(projectModel);
+        }
+
+        // POST: Projects/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ProjectModel projectModel = db.ProjectModels.Find(id);
+            db.ProjectModels.Remove(projectModel);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
