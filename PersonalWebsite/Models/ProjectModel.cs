167e4b211f3f13e63389658aed3944e6cef0ea78
Mirrorperson <Mirrorperson@users.noreply.github.com>
2018-07-01 01:35:31 +0100

    Add new ProjectModel

---
+++ PersonalWebsite/Models/ProjectModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PersonalWebsite.Models
+{
+    public class ProjectModel
+    {
+
+        public int id { get; set; }
+        [Required]
+        public string projectTitle { get; set; }
+        public string projectDescription { get; set; }
+    }
+}
